4371b1b2801aa56efb698530ad49c388bd879c17
ebina <ebina@holyday.co.jp>
2018-09-05 19:42:21 +0900

    no message

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-var sys = require('sys');
-var http = require('http');
-var server = http.createServer(
-    function (request, response) {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write('Hello World!!');
-        response.end();
-    }
-).listen(3000);
-sys.log('Server running at http://localhost:3000/');
+var http = require('http'); //httpモジュール呼び出し
+http.createServer(function (request, response) {
+    // リクエストを受けると以下のレスポンスを送信する
+    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
+    response.end('Hello World\n'); // レスポンスボディに書き込み＆レスポンス送信を完了する
+}).listen(8080);  //8080番ポートで待ち受け
