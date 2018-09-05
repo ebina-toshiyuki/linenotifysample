951745b269e92af74f9976b744287203871b8c42
ebina <ebina@holyday.co.jp>
2018-09-05 19:35:04 +0900

    no message

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-var users = require('./app');
-var notify = require('./notify'); // ←追加
-
-var app = express();
-
-var router = express.Router();
-
-// 中略
-app.use('/app', app);
-app.use('/notify', notify); // ←追加
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {});
-
-
-app.get('/', function(req, res, next) {
-  res.render('notify', { title: 'Express' });
-});
+var sys = require('sys');
+var http = require('http');
+var server = http.createServer(
+    function (request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World!!');
+        response.end();
+    }
+).listen(3000);
+sys.log('Server running at http://localhost:3000/');
