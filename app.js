ad2cfbbbfeb25415658993a4dd62e060eb03b1d2
ebina <ebina@holyday.co.jp>
2018-09-05 19:50:19 +0900

    no message

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+/*
 var http = require('http'); //httpモジュール呼び出し
 http.createServer(function (request, response) {
     // リクエストを受けると以下のレスポンスを送信する
     response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
     response.end('Hello World\n'); // レスポンスボディに書き込み＆レスポンス送信を完了する
 }).listen(process.env.PORT || 8080); //公開ポートで待ち受け
+
+*/
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var POST = process.env.PORT || 8080;
+
+
+//ルートディレクトリにアクセスした時に動く処理
+app.get('/', function(req, res) {
+	//index.htmlに遷移する
+	res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(POST, function() {
+	console.log('接続開始：', POST);
+})
