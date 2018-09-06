67824eb3babaa57a1489696187196477e2a15b8c
ebina <ebina@holyday.co.jp>
2018-09-06 16:48:26 +0900

    no message

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ app.get('/notifyToken', function(req, res) {
 });
 
 app.post('/sendline',function(req, res){
-    
+  console.log(req);
+  console.log(res);
   console.log(req.body);
   const Line = require('./line');
   const myLine = new Line();
