0d87e68387fa0f85081189ee4c42d40632a47c7e
ebina <ebina@holyday.co.jp>
2018-10-01 11:55:05 +0900

    no message

--- app.js
+++ app.js
@@ -130,40 +130,41 @@ app.post('/stripeCreateCus',function(req, res){
             console.log(customer);
             stripe.customers.create(params, function(err,customer){
                 //userData.id = customer.id;
+                console.log(err);
                 console.log(customer);
             });
         }
     });
 
-    // カードの存在チェック
-    stripe.customers.retrieveCard(userData.id, userData.card.ID, function(err, card){
-        if (!card || card.deleted) {
-            // カードが登録されていなければ token を作ってから、customers.createSource() で登録
-            stripe.tokens.create(cardParams, function(err,token){
-                userData.card.ID = token.card.id;
-                userData.card.last4 = token.card.last4;
+    // // カードの存在チェック
+    // stripe.customers.retrieveCard(userData.id, userData.card.ID, function(err, card){
+    //     if (!card || card.deleted) {
+    //         // カードが登録されていなければ token を作ってから、customers.createSource() で登録
+    //         stripe.tokens.create(cardParams, function(err,token){
+    //             userData.card.ID = token.card.id;
+    //             userData.card.last4 = token.card.last4;
     
-                var params = {
-                    source: token.id
-                };
-                stripe.customers.createSource(userData.id, params, function(err, card){
-                    console.log(card);
-                });
-            });
-        } else {
-            // カードが登録されていたら有効期限を更新
-            userData.card.ID = card.id;
-            userData.card.last4 = card.last4;
+    //             var params = {
+    //                 source: token.id
+    //             };
+    //             stripe.customers.createSource(userData.id, params, function(err, card){
+    //                 console.log(card);
+    //             });
+    //         });
+    //     } else {
+    //         // カードが登録されていたら有効期限を更新
+    //         userData.card.ID = card.id;
+    //         userData.card.last4 = card.last4;
     
-            var params = {
-                exp_month: cardParams.card.exp_month,
-                exp_year: cardParams.card.exp_year
-            }
-            stripe.customers.updateCard(userData.ID, card.id, params, function(err, card){
-                console.log(card);
-            });
-        }
-    });
+    //         var params = {
+    //             exp_month: cardParams.card.exp_month,
+    //             exp_year: cardParams.card.exp_year
+    //         }
+    //         stripe.customers.updateCard(userData.ID, card.id, params, function(err, card){
+    //             console.log(card);
+    //         });
+    //     }
+    // });
 
 });
 
